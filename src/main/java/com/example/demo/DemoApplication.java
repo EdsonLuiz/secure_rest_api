4d3ff71d153ae218e33bed04d8906052272c8579
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-04-08 12:13:01 -0300

    Configure JDBCUserDetailsManager

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,16 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -13,15 +18,19 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Bean
-	UserDetailsService userDetailsService() {
-		return new InMemoryUserDetailsManager(
-				User.withDefaultPasswordEncoder()
-					.username("edson")
-					.password("edson")
-					.roles("USER")
-					.build()
-				);
+	UserDetailsService userDetailsService(javax.sql.DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource) {
+			@Override
+			protected List<GrantedAuthority> loadUserAuthorities(String username) {
+				return AuthorityUtils.createAuthorityList("ROLE_USER");
+			}
+		};
 	}
 }
